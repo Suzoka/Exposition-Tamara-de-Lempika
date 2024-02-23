091ba6b10abb1255fd3dff92485be1d35721f567
Suzoka/Morgan <mor9an77@gmail.com>
2024-02-23 16:38:48 +0100

    :sparkles: multiple pages

--- sandbox/formulaire.js
+++ sandbox/formulaire.js
@@ -7,16 +7,39 @@ const datepicker = new Datepicker(document.querySelector('div#date'), {
 let hiddenInput = document.createElement('input');
 hiddenInput.type = 'hidden';
 hiddenInput.name = 'datePicker';
-hiddenInput.required = true;
 document.querySelector('form').appendChild(hiddenInput);
 
 datepicker.element.addEventListener('changeDate', function () {
     hiddenInput.value = datepicker.getDate('yyyy-mm-dd');
 });
 
-document.querySelector('form').addEventListener('submit', function(event) {
-    if (hiddenInput.value == '') {
-        event.preventDefault();
-        alert('Veuillez sélectionner une date.');
-    }
+// document.querySelector('form').addEventListener('submit', function(event) {
+//     if (hiddenInput.value == '') {
+//         event.preventDefault();
+//         alert('Veuillez sélectionner une date.');
+//     }
+// });
+
+
+document.querySelectorAll("div.heure label").forEach(label => {
+    label.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter' || event.keyCode === 13) {
+            let radioButton = document.querySelector('#' + this.getAttribute('for'));
+            radioButton.checked = true;
+        }
+    });
+});
+
+document.querySelectorAll("button.confirmer").forEach(button => {
+    button.addEventListener('click', function(event) {
+        document.querySelector('#' + event.target.parentElement.parentElement.getAttribute('id')).style.display = 'none';
+        document.querySelector('#step' + (parseInt(event.target.parentElement.parentElement.getAttribute('id').replace("step",""))+1)).style.display = 'block';
+    });
+});
+
+document.querySelectorAll("button.retour").forEach(button => {
+    button.addEventListener('click', function(event) {
+        document.querySelector('#' + event.target.parentElement.parentElement.getAttribute('id')).style.display = 'none';
+        document.querySelector('#step' + (parseInt(event.target.parentElement.parentElement.getAttribute('id').replace("step",""))-1)).style.display = 'block';
+    });
 });
