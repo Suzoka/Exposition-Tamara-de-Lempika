e82233b7e7efb5ee02c4c674e0ca3f19965d6684
Lou-AnneDub <dubillelouanne@gmail.com>
2024-03-04 12:33:27 +0100

    :construction: :pencil2: Header + ajout des fonts

---
+++ views/accueil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/style_header.css">
+    <title>Expo Tamara de Lempicka - Les années folles</title>
+</head>
+<body>
+    <?php include 'components/header.php'; ?>
+    
+</body>
+</html>
