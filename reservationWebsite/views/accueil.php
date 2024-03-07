056b98b8db45cb46ff4aa182e715e41185dc8e15
Lou-AnneDub <dubillelouanne@gmail.com>
2024-03-07 11:07:49 +0100

    :construction: banniere + new hierarchisation

--- reservationWebsite/views/accueil.php
+++ reservationWebsite/views/accueil.php
@@ -18,6 +18,17 @@
             </div>
             <img src="../img/TamaraPaint.png" alt="">
         </section>
+
+
+        <?php include 'components/header.php'; ?>
+        <section class="banniere">
+            <div class="text">
+                <h1><span id="tam">Tamara</span>
+                <br>de Lempicka, <span id="era">les annees folles</span></h1>
+                <p>Une exposition virtuelle dans les temps des années folles à travers les tableaux décalés et interrogateurs de Tamara </p>
+            </div>
+            <img src="../img/TamaraPaint2.png" alt="">
+        </section>
     </main>    
     
 </body>
