75209c166dce6cba15e43f3766e1ef7c26d8ba3b
Lou-AnneDub <dubillelouanne@gmail.com>
2024-03-07 20:22:16 +0100

    :construction: fin teaser (manque typo)

--- reservationWebsite/views/accueil.php
+++ reservationWebsite/views/accueil.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../styles/style_header.css">
-    <link rel="stylesheet" href="../styles/style_accueil.css">
+<link rel="stylesheet" href="../styles/style_header.css">
+ <link rel="stylesheet" href="../styles/style_accueil.css"> 
     <title>Expo Tamara de Lempicka - Les années folles</title>
 </head>
 <body>
@@ -16,21 +16,36 @@
                 <br>de Lempicka, <span id="era">les annees folles</span></h1>
                 <p>Une exposition virtuelle dans les temps des années folles à travers les tableaux décalés et interrogateurs de Tamara </p>
             </div>
-            <img src="../img/TamaraPaint.png" alt="">
+            <div class="imgBanniere">
+                <img id="tamaraPaint" src="../img/TamaraPaint.png" alt="">
+            </div>
+            <a href="#teaser" class="scroll">
+                <img src="../img/scroll.svg" alt="scroll">
+            </a>
         </section>
-        <section class="teaser">
+        <section class="teaser" id="teaser">
             <h2>Teaser</h2>
             <div class="contentTeaser">
                 <div class="iframeResponsive">
                     <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" ></iframe>
                 </div>
-                <div class="present">
-                    <div class="info">
-                        <img src="./img/place.svg" alt="">
-                        <p>IUT de Marne-la-Vallée</p>
+                <div class="presentation">
+                    <div class="infos">
+                        <div class="info">
+                            <img src="../img/place.svg" alt="">
+                            <p>IUT de Marne-la-Vallée</p>
+                        </div>
+                        <div class="info">
+                            <img src="../img/time.svg" alt="">
+                            <p>30min</p>
+                        </div>
                     </div>
-                    <p>Plongez à la découverte des années folles à travers Tamara de Lempicka et ses tableaux ! Cette époque où l’art moderne et la représentation de la femme prennent vie. Les années folles symbole, d’une époque unique, euphorique, où le domaine artistique et culturelle connaissent leur apogée.
+                    <p id="textPresent">Plongez à la découverte des années folles à travers Tamara de Lempicka et ses tableaux ! Cette époque où l’art moderne et la représentation de la femme prennent vie. Les années folles symbole, d’une époque unique, euphorique, où le domaine artistique et culturelle connaissent leur apogée.
                     <br>Des œuvres qui illustrent l’émancipation de la femme et la beauté féminine. Tamara y peint une image de la femme libre, autonome et épanoui dans sa vie et ses propres choix. Grâce à cette exposition interactive unique, vous aurez la chance de vous inviter dans l’univers de Tamara et de découvrir les tableaux d’une toute nouvelle manière. </p>
+                    <a id="reservation" href="">
+                        <img src="../img/ticket.svg" alt="">
+                        <p>Réserver</p>
+                    </a>
                 </div>
             </div>
             
