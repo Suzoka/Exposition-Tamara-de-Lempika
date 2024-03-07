42566136b66563cddff80f1bd37acb84cb6bf1ae
Lou-AnneDub <dubillelouanne@gmail.com>
2024-03-07 16:49:43 +0100

    :construction: partie teaser

--- reservationWebsite/views/accueil.php
+++ reservationWebsite/views/accueil.php
@@ -18,16 +18,23 @@
             </div>
             <img src="../img/TamaraPaint.png" alt="">
         </section>
-
-
-        <?php include 'components/header.php'; ?>
-        <section class="banniere">
-            <div class="text">
-                <h1><span id="tam">Tamara</span>
-                <br>de Lempicka, <span id="era">les annees folles</span></h1>
-                <p>Une exposition virtuelle dans les temps des années folles à travers les tableaux décalés et interrogateurs de Tamara </p>
+        <section class="teaser">
+            <h2>Teaser</h2>
+            <div class="contentTeaser">
+                <div class="iframeResponsive">
+                    <iframe src="https://www.youtube.com/embed/dQw4w9WgXcQ" ></iframe>
+                </div>
+                <div class="present">
+                    <div class="info">
+                        <img src="./img/place.svg" alt="">
+                        <p>IUT de Marne-la-Vallée</p>
+                    </div>
+                    <p>Plongez à la découverte des années folles à travers Tamara de Lempicka et ses tableaux ! Cette époque où l’art moderne et la représentation de la femme prennent vie. Les années folles symbole, d’une époque unique, euphorique, où le domaine artistique et culturelle connaissent leur apogée.
+                    <br>Des œuvres qui illustrent l’émancipation de la femme et la beauté féminine. Tamara y peint une image de la femme libre, autonome et épanoui dans sa vie et ses propres choix. Grâce à cette exposition interactive unique, vous aurez la chance de vous inviter dans l’univers de Tamara et de découvrir les tableaux d’une toute nouvelle manière. </p>
+                </div>
             </div>
-            <img src="../img/TamaraPaint2.png" alt="">
+            
+
         </section>
     </main>    
     
