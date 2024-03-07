af30d86cc68d2e4351c59339264e64daef23d316
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-07 11:08:21 +0100

    arborescence

---
+++ reservationWebsite/scripts/database.php
@@ -0,0 +1,17 @@
+<?php
+function autoLoad($classe)
+{
+    require 'class/' . $classe . '.php';
+}
+
+spl_autoload_register('autoLoad'); 
+
+$serveur = "localhost";
+$login = "root";
+$password = "";
+$bdd = "resaexpo";
+
+$db = new PDO("mysql:host=$serveur;dbname=$bdd;charset=utf8", $login, $password);
+
+$manager = new Manager($db);
+?>
