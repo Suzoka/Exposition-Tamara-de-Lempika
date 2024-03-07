ff54f083e93f90ea2a60dc3da3819b73dec5aa5a
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-07 17:37:49 +0100

    :sparkles: create reservations

--- reservationWebsite/scripts/class/Manager.php
+++ reservationWebsite/scripts/class/Manager.php
@@ -23,6 +23,29 @@ class Manager
         }
         return $listeFormules;
     }
+
+    public function createReservation($datas)
+    {
+        $id = 1;
+        $date = $datas['datePicker'] . " " . $datas['heure'];
+
+        foreach ($datas as $key => $value) {
+            if (strpos($key, 'formule') !== false) {
+                $id_formule = substr($key, 7);
+
+                for ($i = 0; $i < $value; $i++) {
+                    $stmt = $this->db->prepare("INSERT INTO `reservations` (`ext_id_user`, `ext_id_formule`, `date`) VALUES (:ext_id_user, :ext_id_formule, :date)");
+
+                    //Changer par l'id stocké en session
+                    $stmt->bindParam(':ext_id_user', $id, PDO::PARAM_INT);
+
+                    $stmt->bindParam(':ext_id_formule', $id_formule, PDO::PARAM_INT);
+                    $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+                    $stmt->execute();
+                }
+            }
+        }
+    }
 }
 
 ?>
