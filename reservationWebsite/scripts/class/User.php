fb3fbcb6479457b5fef92f786eb3552caeadfdc0
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-08 16:56:06 +0100

    add session management

---
+++ reservationWebsite/scripts/class/User.php
@@ -0,0 +1,96 @@
+<?php 
+class User {
+    private $id_user;
+    private $username;
+    private $mail;
+    private $password;
+    private $role;
+    private $nom;
+    private $prenom;
+
+    public function getId_user()
+    {
+        return $this->id_user;
+    }
+
+    public function setId_user($id_user)
+    {
+        $this->id_user = $id_user;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function hydrate($data)
+    {
+        foreach ($data as $key => $value) {
+            $method = 'set' . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function __construct($data)
+    {
+        $this->hydrate($data);
+    }
+}
+?>
