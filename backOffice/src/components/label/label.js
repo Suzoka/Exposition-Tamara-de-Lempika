74c473d7c3345bf350f4783ed5e521dd6be8a11c
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-11 22:19:08 +0100

    💄 mirco ajustement label

--- backOffice/src/components/label/label.js
+++ backOffice/src/components/label/label.js
@@ -4,9 +4,9 @@ export const Label = ({ children, variant }) => {
     
     const className = `label__container 
     ${
-        variant === 'adulte' ? 'label__container--blue' 
-        : variant === 'jeune' ? 'label__container--green' 
-        : variant === 'handicap' ? 'label__container--yellow' 
+        variant === 'Adulte' ? 'label__container--blue' 
+        : variant === 'Jeune' ? 'label__container--green' 
+        : variant === 'Handicap' ? 'label__container--yellow' 
         : ''
     }`;
 
