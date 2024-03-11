c74469de57fc45a7a9c3a2916edf907e2ea6c2cc
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-11 22:19:29 +0100

    ✨ Componement table

--- backOffice/src/app/page.js
+++ backOffice/src/app/page.js
@@ -1,15 +1,54 @@
+import Image from 'next/image';
+import Trash  from '../../public/asset/icon/Trash-2.svg';
+import Eye  from '../../public/asset/icon/Eye.svg';
+
+import { Button } from "@/components/button/button";
 import { Date } from "@/components/date/date";
 import { Label } from "@/components/label/label";
+import { Table } from '@/components/table/table';
+
+const reservation = [
+  {
+    id: 1,
+    img: "https://awarewomenartists.com/wp-content/uploads/2017/05/portrait_tamara-de-lempicka_aware_women-artists_artistes-femmes-1076x1500.jpg",
+    utilisateur: "Lorem Leseul",
+    date: "07/03/2024",
+    creneau: "10:30",
+    formule: "Adulte",
+    mail: "lorem.leseul@gmail.com",
+    prix: "15",
+    quantite: 2
+  },
+  {
+    id: 2,
+    img: "https://awarewomenartists.com/wp-content/uploads/2017/05/portrait_tamara-de-lempicka_aware_women-artists_artistes-femmes-1076x1500.jpg",
+    utilisateur: "Jaqueline Leseul",
+    date: "07/03/2024",
+    creneau: "10:30",
+    formule: "Jeune",
+    mail: "jaqueline@gmail.com",
+    prix: "10",
+    quantite: 1
+  },
+  {
+    id: 3,
+    img: "https://awarewomenartists.com/wp-content/uploads/2017/05/portrait_tamara-de-lempicka_aware_women-artists_artistes-femmes-1076x1500.jpg",
+    utilisateur: "Jean Leseul",
+    date: "07/03/2024",
+    creneau: "10:30",
+    formule: "Handicap",
+    mail: "leseul7750@gmail.com",
+    prix: "5",
+    quantite: 1
+  }
+]
 
 export default function Home() {
   return (
-    <main>
+    <main> 
       <h1>Home</h1>
-      < Label variant="adulte">Adulte</Label>
-      < Label variant="jeune">Jeune</Label>
-      < Label variant="handicap">Handicap</Label>
-      < Date variant="day">07/03/2024</Date>
-      < Date variant="hour">10:30</Date>
+      <h2>Reservation</h2>
+      < Table donnee={reservation}/>
     </main>
   );
 }
---
+++ backOffice/src/components/table/table.js
@@ -0,0 +1,31 @@
+import { Table_object } from "../table_object/table_object";
+import classes from './table.module.css';
+
+export const Table = ({donnee}) => {
+
+    return (
+        <table>
+            <thead>
+                <tr className={classes['table__head']}>
+                    <th>Id</th>
+                    <th>Utilisateur</th>
+                    <th>Date</th>
+                    <th>Créneau</th>
+                    <th>Formule</th>
+                    <th>Mail</th>
+                    <th>Prix</th>
+                    <th>Quantité</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    donnee.map((item, index) => {
+                        return <Table_object key={index} liste={item} />
+                    })
+                }
+            </tbody>
+        </table>
+    );
+
+}
