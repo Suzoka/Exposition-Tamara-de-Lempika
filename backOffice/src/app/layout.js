8a0df6b20b0c63523b1b1fbe345fb0826347bfd4
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-12 12:29:12 +0100

    🙈 blocage de l'indexation

--- backOffice/src/app/layout.js
+++ backOffice/src/app/layout.js
@@ -6,12 +6,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Back office - TDL exposition",
   favicon: "./favicon.ico",
+  
   // description: " ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <meta name="robots" content="noindex" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
