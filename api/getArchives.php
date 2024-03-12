b504b79a2cbec11bb93d5fe9f8243b472cfa5c96
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-12 15:27:39 +0100

    �:lock: secure users datas with JWT

--- api/getArchives.php
+++ api/getArchives.php
@@ -2,26 +2,41 @@
 include "api_model.php";
 $request_method = $_SERVER["REQUEST_METHOD"];
 
-switch ($request_method) {
+$jwt = new JWT();
 
-    case 'GET':
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
 
-        if (!isset($parts[4]) || $parts[4] == null) {
-            $result = getAllArchives()->fetchAll(PDO::FETCH_ASSOC);
-        } else {
-            $search = $parts[4];
-            $result = searchArchives($search)->fetchAll(PDO::FETCH_ASSOC);
+    $token = str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']);
+    if ($jwt->check($token, SECRET) && !$jwt->isExpired($token) && $jwt->isValid($token)) {
+
+        switch ($request_method) {
+
+            case 'GET':
+
+                if (!isset($parts[4]) || $parts[4] == null) {
+                    $result = getAllArchives()->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    $search = $parts[4];
+                    $result = searchArchives($search)->fetchAll(PDO::FETCH_ASSOC);
+                }
+
+                if ($result == null) {
+                    http_response_code(404);
+                    echo json_encode(["message" => "Aucun résultat n'a été trouvé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                } else {
+                    echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                }
+                break;
+            default:
+                echo json_encode(["message" => "Méthode non autorisée"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                break;
         }
-
-        if ($result == null) {
-            http_response_code(404);
-            echo json_encode(["message" => "Aucun résultat n'a été trouvé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        } else {
-            echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        }
-        break;
-    default:
-        echo json_encode(["message" => "Méthode non autorisée"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        break;
+    } else {
+        http_response_code(403);
+        echo json_encode(["message" => "Accès refusé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+} else {
+    http_response_code(403);
+    echo json_encode(["message" => "Veuillez renseigner un token"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
 ?>
--- api/reservationsActions.php
+++ api/reservationsActions.php
@@ -2,33 +2,35 @@
 include "api_model.php";
 $request_method = $_SERVER["REQUEST_METHOD"];
 
-switch ($request_method) {
+require_once 'includes/config.php';
+require_once 'classes/JWT.php';
 
-    case 'GET':
+$jwt = new JWT();
 
-        if (!isset($parts[4]) || $parts[4] == null) {
-            $result = getAllReservedFormulesNotArchived()->fetchAll(PDO::FETCH_ASSOC);
-        } else {
-            $search = $parts[4];
-            $result = searchReservedFormulesNotArchived($search)->fetchAll(PDO::FETCH_ASSOC);
-        }
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
 
-        if ($result == null) {
-            http_response_code(404);
-            echo json_encode(["message" => "Aucun résultat n'a été trouvé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        } else {
-            echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        }
-        break;
-    case 'DELETE':
-        require_once 'includes/config.php';
-        require_once 'classes/JWT.php';
+    $token = str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']);
+    if ($jwt->check($token, SECRET) && !$jwt->isExpired($token) && $jwt->isValid($token)) {
 
-        $jwt = new JWT();
+        switch ($request_method) {
 
-        if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
-            $token = str_replace('Bearer ', '', $_SERVER['HTTP_AUTHORIZATION']);
-            if ($jwt->check($token, SECRET) && !$jwt->isExpired($token) && $jwt->isValid($token)) {
+            case 'GET':
+
+                if (!isset($parts[4]) || $parts[4] == null) {
+                    $result = getAllReservedFormulesNotArchived()->fetchAll(PDO::FETCH_ASSOC);
+                } else {
+                    $search = $parts[4];
+                    $result = searchReservedFormulesNotArchived($search)->fetchAll(PDO::FETCH_ASSOC);
+                }
+
+                if ($result == null) {
+                    http_response_code(404);
+                    echo json_encode(["message" => "Aucun résultat n'a été trouvé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                } else {
+                    echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                }
+                break;
+            case 'DELETE':
                 if (isset($parts[4]) && $parts[4] != null) {
                     $id = $parts[4];
                     if (deleteReservation($id)) {
@@ -39,17 +41,17 @@ switch ($request_method) {
                 } else {
                     echo json_encode(["message" => "Veuillez indiquer un ID"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
                 }
-            } else {
-                http_response_code(403);
-                echo json_encode(["message" => "Accès refusé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-            }
-        } else {
-            http_response_code(403);
-            echo json_encode(["message" => "Veuillez renseigner un token"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                break;
+            default:
+                echo json_encode(["message" => "Méthode non autorisée"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+                break;
         }
-        break;
-    default:
-        echo json_encode(["message" => "Méthode non autorisée"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        break;
+    } else {
+        http_response_code(403);
+        echo json_encode(["message" => "Accès refusé"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    }
+} else {
+    http_response_code(403);
+    echo json_encode(["message" => "Veuillez renseigner un token"], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 }
 ?>
