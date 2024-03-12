750251222a36eb8d47e198b1fd1432d44c8724e0
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-12 12:30:21 +0100

    🐛 régalge classname

--- backOffice/src/components/label/label.js
+++ backOffice/src/components/label/label.js
@@ -2,8 +2,7 @@ import './label.css';
 
 export const Label = ({ children, variant }) => {
     
-    const className = `label__container 
-    ${
+    const className = `label__container ${
         variant === 'Adulte' ? 'label__container--blue' 
         : variant === 'Jeune' ? 'label__container--green' 
         : variant === 'Handicap' ? 'label__container--yellow' 
