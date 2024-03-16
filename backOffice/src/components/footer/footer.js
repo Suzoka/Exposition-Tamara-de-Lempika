91fbb6c76eb470be25dbcec3ee1fa01355e57f15
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-16 16:35:04 +0100

    🚧 Footer

--- backOffice/src/components/footer/footer.js
+++ backOffice/src/components/footer/footer.js
@@ -1,6 +1,8 @@
+import classes from './footer.module.css';
+
 export const Footer = () => {
     return (
-        <footer>
+        <footer className={classes['footer__container']}>
             <a href="https://sinyart.fr">Site de l'agence Siny'art</a>
             <a href="https://expotamara.fr">L'exposition Tamara de Lempicka</a>
         </footer>
