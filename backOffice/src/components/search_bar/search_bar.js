3b78beb11d136895c89ab04e7a920884c3fab928
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-16 16:41:41 +0100

    🐛 search bar no name

--- backOffice/src/components/search_bar/search_bar.js
+++ backOffice/src/components/search_bar/search_bar.js
@@ -4,7 +4,7 @@ export const SearchBar = ({content}) => {
     return (
         <form className={classes['searchbar__container']}>
             {/* <input type="submit" value="loupe" /> */}
-            <input type="text" placeholder={content} className={classes['searchbar__input']}/>
+            <input type="text" name="recherche" placeholder={content} className={classes['searchbar__input']}/>
         </form>
     );
 };
