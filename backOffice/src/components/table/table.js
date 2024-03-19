d5916424f0a39d108a331dcce0c342b6d4cd49ef
Robin Vigier / B!nro <rob.vigier@gmail.com>
2024-03-19 11:56:51 +0100

    👽️ ✨ API utilisateur

--- backOffice/src/components/table/table.js
+++ backOffice/src/components/table/table.js
@@ -1,30 +1,42 @@
 import { Table_object } from "../table_object/table_object";
 import classes from './table.module.css';
 
-export const Table = ({donnee, DelModViewResa, variant}) => {
+export const Table = ({ donnee, DelModViewResa, variant }) => {
 
     return (
         <section className={classes['table__container']}>
             <table className={classes['table__object']}>
                 <thead>
-                    <tr className={classes['table__head']}>
-                        <th>Id</th>
-                        <th>Utilisateur</th>
-                        <th>Date</th>
-                        <th>Créneau</th>
-                        <th>Formule</th>
-                        <th>Mail</th>
-                        <th>Prix</th>
-                        <th>Quantité</th>
-                        { variant==='modification' ? (
-                            <th>Action</th>
-                        ) : ''}
-                    </tr>
+                    {variant === 'modification' || variant === '' ? (
+                        <tr className={classes['table__head']}>
+                            <th className={classes['table__head--Id']}>Id</th>
+                            <th className={classes['table__head--User']}>Utilisateur</th>
+                            <th className={classes['table__head--Date']}>Date</th>
+                            <th className={classes['table__head--Creneau']}>Créneau</th>
+                            <th className={classes['table__head--Formule']}>Formule</th>
+                            <th className={classes['table__head--Mail']}>Mail</th>
+                            <th className={classes['table__head--Prix']}>Prix</th>
+                            <th className={classes['table__head--Quantite']}>Quantité</th>
+                            {variant === 'modification' ? (
+                                <th className={classes['table__head--Action']}>Action</th>
+                            ) : ''}
+                        </tr>
+                    ) : variant==='user' ? (
+                        <tr className={classes['table__head']}>
+                            <th className={classes['table__head--Id']}>Id</th>
+                            <th className={classes['table__head--User']}>Utilisateur</th>
+                            <th className={classes['table__head--Date']}>Login</th>
+                            <th className={classes['table__head--Mail']}>Mail</th>
+                            <th className={classes['table__head--Formule']}>Rôle</th>
+                            <th className={classes['table__head--Action']}>Action</th>
+                        </tr>
+                    ) : ''}
+
                 </thead>
                 <tbody>
                     {
                         donnee.map((item, index) => {
-                            return <Table_object key={index} liste={item} DelModViewResa={DelModViewResa} variant={variant==='modification' ? 'modification' : ''} />
+                            return <Table_object key={index} liste={item} DelModViewResa={DelModViewResa} variant={variant} />
                         })
                     }
                 </tbody>
--- backOffice/src/components/table_object/table_object.js
+++ backOffice/src/components/table_object/table_object.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
-import Trash  from '../../../public/asset/icon/Trash-2.svg';
-import Eye  from '../../../public/asset/icon/Eye.svg';
+import Trash from '../../../public/asset/icon/Trash-2.svg';
+import Eye from '../../../public/asset/icon/Eye.svg';
 
 import { Button } from "@/components/button/button";
 import { DatePills } from "@/components/datePills/datePills";
@@ -11,61 +11,103 @@ import classes from './table_object.module.css';
 
 export const Table_object = ({ liste, variant, DelModViewResa }) => {
 
-    let formattedId = '# ' + liste.id_ticket;
+    let formattedId = '# ' + (liste.id_ticket ? liste.id_ticket : liste.id_user);
     const date = new Date(liste.date);
-    let Jour =`${date.getDate()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
-    let Heure =`${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
+    let Jour = `${date.getDate()}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+    let Heure = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`;
 
     return (
         <tr className={classes['table__ligne']}>
-            <td> 
-                {/* ID de la réséervation */}
-                {formattedId}
-            </td>
-            <td className={classes['table__cellule--user']}>
-                {/* Utilisateur */}
-                <img src={liste.img} className={classes['user__icon']} alt=""/>
-                {liste.prenom} {liste.nom}
-            </td>
-            <td>
-                {/* Date de la réservation */}
-                < DatePills variant="day">{Jour}</DatePills>
-            </td>
-            <td>
-                {/* Créneau réservé */}
-                < DatePills variant="hour">{Heure}</DatePills>
-            </td>
-            <td>
-                {/* Formule choisie */}
-                < Label variant={liste.nom_formule}>{liste.nom_formule}</Label>
-            </td>
-            <td>
-                {/* Mail */}
-                {liste.mail}
-            </td>
-            <td>
-                {/* Prix */}
-                {liste.prix}€
-            </td>
-            <td>
-                {/* Quantité */}
-                x {liste.quantite}
-            </td>
+            {variant === 'modification' || variant === '' ? (
+                <>
+                    <td>
+                        {/* ID de la réséervation */}
+                        {formattedId}
+                    </td>
+                    <td className={classes['table__cellule--user']}>
+                        {/* Utilisateur */}
+                        <img src={liste.img} className={classes['user__icon']} alt="" />
+                        {liste.prenom} {liste.nom}
+                    </td>
+                    <td>
+                        {/* Date de la réservation */}
+                        < DatePills variant="day">{Jour}</DatePills>
+                    </td>
+                    <td>
+                        {/* Créneau réservé */}
+                        < DatePills variant="hour">{Heure}</DatePills>
+                    </td>
+                    <td>
+                        {/* Formule choisie */}
+                        < Label variant={liste.nom_formule}>{liste.nom_formule}</Label>
+                    </td>
+                    <td>
+                        {/* Mail */}
+                        {liste.mail}
+                    </td>
+                    <td>
+                        {/* Prix */}
+                        {liste.prix}€
+                    </td>
+                    <td>
+                        {/* Quantité */}
+                        x {liste.quantite}
+                    </td>
 
-            {/* Action */}
-            {variant === 'modification' && (
-                <td className={classes['table__cellule--action']}>
-                    < Button variant="primary" title="Modifier" idElement={liste.id_ticket} action={DelModViewResa['modifyResa']}>Modifier</Button>
+                    {/* Action */}
+                    {variant === 'modification' && (
+                        <td className={classes['table__cellule--action']}>
+                            < Button variant="primary" title="Modifier" idElement={liste.id_ticket} action={DelModViewResa['modifyResa']}>Modifier</Button>
 
-                    < Button format="icon" title="Suprimer" icon="trash" idElement={liste.id_ticket} action={DelModViewResa['deleteResa']}>
-                        <span className='icon_alt'>Supprimer</span>
-                    </Button>
+                            < Button format="icon" title="Suprimer" icon="trash" idElement={liste.id_ticket} action={DelModViewResa['deleteResa']}>
+                                <span className='icon_alt'>Supprimer</span>
+                            </Button>
 
-                    < Button format="icon" title="Voir" icon="eye" idElement={liste.id_ticket} action={DelModViewResa['viewResa']}>
-                        <span className='icon_alt'>Voir</span>
-                    </Button>
-                </td>
-            )}
+                            < Button format="icon" title="Voir" icon="eye" idElement={liste.id_ticket} action={DelModViewResa['viewResa']}>
+                                <span className='icon_alt'>Voir</span>
+                            </Button>
+                        </td>
+                    )}
+                </>
+            ) : variant === 'user' ? (
+                <>
+                    <td>
+                        {/* ID de l'utilisateur */}
+                        {formattedId}
+                    </td>
+                    <td className={classes['table__cellule--user']}>
+                        {/* Utilisateur */}
+                        <img src={liste.img} className={classes['user__icon']} alt="" />
+                        {liste.prenom} {liste.nom}
+                    </td>
+                    <td>
+                        {/* Login */}
+                        {liste.username}
+                    </td>
+                    <td>
+                        {/* Mail */}
+                        {liste.mail}
+                    </td>
+                    <td>
+                        {/* Rôle */}
+                        {liste.role === 1 ? 'Admin' : liste.role === 0 ? 'Client' : ''}
+                    </td>
+
+                    {/* Action */}
+                    <td className={classes['table__cellule--action']}>
+                        < Button variant="primary" title="Modifier" idElement={liste.id_ticket} action={DelModViewResa['modifyResa']}>Modifier</Button>
+
+                        < Button format="icon" title="Suprimer" icon="trash" idElement={liste.id_ticket} action={DelModViewResa['deleteResa']}>
+                            <span className='icon_alt'>Supprimer</span>
+                        </Button>
+
+                        < Button format="icon" title="Voir" icon="eye" idElement={liste.id_ticket} action={DelModViewResa['viewResa']}>
+                            <span className='icon_alt'>Voir</span>
+                        </Button>
+                    </td>
+                </>
+            ) : ''
+            }
         </tr>
     )
 
