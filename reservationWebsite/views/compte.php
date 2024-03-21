6b815c31a95d4c890b4ce37b71276112af5b9988
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-21 10:17:55 +0100

    :sparkles: connexion popup account space

--- reservationWebsite/views/compte.php
+++ reservationWebsite/views/compte.php
@@ -11,13 +11,26 @@
 
 <body>
     <?php include 'components/header.php'; ?>
-    <main>
+    <main <?php if (!isset ($_SESSION['user'])) {
+        echo 'class="notLoged"';
+    } ?>>
+        <?php if (!isset ($_SESSION['user'])) { ?>
+            <div class="popup">
+                <h2>Connectez-vous ou inscrivez-vous pour accéder à votre compte</h2>
+                <div class="link">
+                    <a href="./connexion">Connexion</a>
+                    <a href="./inscription">Inscription</a>
+                </div>
+            </div>
+        <?php } ?>
         <h1>Mon compte</h1>
         <img id="compte" src="../img/compte.png" alt="">
-        <h2>
-            <?php echo $_SESSION['user']->getPrenom() ?>
-            <?php echo $_SESSION['user']->getNom() ?>
-        </h2>
+        <?php if (isset ($_SESSION['user'])) { ?>
+            <h2>
+                <?php echo $_SESSION['user']->getPrenom() ?>
+                <?php echo $_SESSION['user']->getNom() ?>
+            </h2>
+        <?php } ?>
         <section class="change">
             <div class="boutons">
                 <a class="<?php if ($activePage == "infos") {
@@ -31,15 +44,17 @@
             </div>
             <?php if ($activePage == "infos") { ?>
                 <div class="infos">
-                    <p>Nom :
-                        <?php echo $_SESSION['user']->getNom() ?>
-                    </p>
-                    <p>Prénom :
-                        <?php echo $_SESSION['user']->getPrenom() ?>
-                    </p>
-                    <p>Adresse mail :
-                        <?php echo $_SESSION['user']->getMail() ?>
-                    </p>
+                    <?php if (isset ($_SESSION['user'])) { ?>
+                        <p>Nom :
+                            <?php echo $_SESSION['user']->getNom() ?>
+                        </p>
+                        <p>Prénom :
+                            <?php echo $_SESSION['user']->getPrenom() ?>
+                        </p>
+                        <p>Adresse mail :
+                            <?php echo $_SESSION['user']->getMail() ?>
+                        </p>
+                    <?php } ?>
                 </div>
             <?php } else { ?>
                 <div class="reservations">
@@ -47,44 +62,47 @@
                     <div class="billets">
 
                         <?php
-                        $tickets = $manager->getReservationsOfUser($_SESSION['user']->getId_user());
-                        if ($tickets->rowCount() == 0) {
-                            echo "<p>Vous n'avez pas encore de réservation</p>
+                        if (isset ($_SESSION['user'])) {
+                            $tickets = $manager->getReservationsOfUser($_SESSION['user']->getId_user());
+                            if ($tickets->rowCount() == 0) {
+                                echo "<p>Vous n'avez pas encore de réservation</p>
                             <a id=\"reservation\" href=\"./billetterie\">
                             <img src=\"../img/icons/ticket.svg\" alt=\"\">
                             <p>Réserver maintenant</p>
                         </a>";
-                        } else {
-                            foreach ($tickets->fetchAll(PDO::FETCH_ASSOC) as $key => $value) {
-                                ?>
-                                <div class="billet">
-                                    <div class="timeDates">
-                                        <div class="date">
-                                            <p>le
-                                                <?php $date = new DateTime($value['date']);
-                                                $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
-                                                echo $formatter->format($date); ?>
-                                            </p>
+                            } else {
+                                foreach ($tickets->fetchAll(PDO::FETCH_ASSOC) as $key => $value) {
+                                    ?>
+                                    <div class="billet">
+                                        <div class="timeDates">
+                                            <div class="date">
+                                                <p>le
+                                                    <?php $date = new DateTime($value['date']);
+                                                    $formatter = new IntlDateFormatter('fr_FR', IntlDateFormatter::LONG, IntlDateFormatter::NONE);
+                                                    echo $formatter->format($date); ?>
+                                                </p>
+                                            </div>
+                                            <div class="time">
+                                                <p>à
+                                                    <?php echo str_replace(":", "h", $date->format('H:i')) ?>
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div class="time">
-                                            <p>à
-                                                <?php echo str_replace(":", "h", $date->format('H:i')) ?>
-                                            </p>
-                                        </div>
+                                        <h4>Billet
+                                            <?php echo $value['nom_formule'] ?>
+                                            x
+                                            <?php echo $value['quantite'] ?>
+                                        </h4>
+                                        <p>
+                                            <?php echo $value['explication_formule'] ?>
+                                        </p>
+                                        <p class="price">
+                                            <?php echo $value['tarif'] == 0 ? "Gratuit" : $value['tarif'] * $value['quantite'] ?>
+                                        </p>
                                     </div>
-                                    <h4>Billet
-                                        <?php echo $value['nom_formule'] ?>
-                                        x<?php echo $value['quantite'] ?>
-                                    </h4>
-                                    <p>
-                                        <?php echo $value['explication_formule'] ?>
-                                    </p>
-                                    <p class="price">
-                                        <?php echo $value['tarif'] == 0 ? "Gratuit" : $value['tarif'] * $value['quantite'] ?>
-                                    </p>
-                                </div>
 
-                            <?php }
+                                <?php }
+                            }
                         } ?>
 
 
