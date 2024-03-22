69f12c7ce0469408d99d9d438394e9344b80c11e
Lou-AnneDub <dubillelouanne@gmail.com>
2024-03-22 16:14:07 +0100

    pp profil change

--- reservationWebsite/views/components/header.php
+++ reservationWebsite/views/components/header.php
@@ -9,7 +9,7 @@
             <a href="./billetterie"><?php echo $_SESSION["lang"] == "fr" ? "Billetterie" : "Ticketing" ?><img src="../img/icons/arrow.svg" alt="Allez à la section"></a>
         </div>
         <div class="menu-user">
-            <?php echo isset ($_SESSION["user"]) ? "<a href=\"./deconnexion\">". ($_SESSION["lang"]=="fr" ? "Se déconnecter" : "Sign out") . "</a><a id=\"compte\" href=\"./compte\"><img src=\"../img/compte.png\" alt=\"".($_SESSION["lang"]=="fr" ? "Mon compte" : "My account")."\"></a>" : "<a href=\"./connexion\">".($_SESSION["lang"]=="fr" ? "Se connecter" : "Sign in")."</a>
+            <?php echo isset ($_SESSION["user"]) ? "<a href=\"./deconnexion\">". ($_SESSION["lang"]=="fr" ? "Se déconnecter" : "Sign out") . "</a><a id=\"compte\" href=\"./compte\"><img src=\"../img/icons/profil.svg\" alt=\"".($_SESSION["lang"]=="fr" ? "Mon compte" : "My account")."\"></a>" : "<a href=\"./connexion\">".($_SESSION["lang"]=="fr" ? "Se connecter" : "Sign in")."</a>
                 <a href=\"./inscription\">".($_SESSION["lang"]=="fr" ? "S'inscrire" : "Sign up")."</a>" ?>
             <?php if ($_SESSION["lang"] == "fr") { ?>
                 <a href="./languageEN?from=<?php echo $page ?>"><img class="language" src="../img/icons/en.svg" alt="change language to english" lang="en"></a>
