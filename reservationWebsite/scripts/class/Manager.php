40a9e12ada0073f16472d0bd6d65d66edb75bb2f
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-26 14:58:45 +0100

    mail signature

--- reservationWebsite/scripts/class/Manager.php
+++ reservationWebsite/scripts/class/Manager.php
@@ -59,14 +59,14 @@ class Manager
                 mail(
                     $datas['mail'],
                     mb_encode_mimeheader("Réservation(s) effectuée(s)", 'UTF-8', 'B', "\r\n"),
-                    "Bonjour " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nNous vous confirmons la réservation des formules suivantes : \n" . $reservations . "\nNous vous attendons le " . $formatter->format(new DateTime($datas['datePicker'])) . " à " . DateTime::createFromFormat('H:i:s', $datas['heure'])->format('H\hi') . " pour profiter de votre séjour.\n\nCordialement,\nL'équipe de chez Siny'art",
+                    "Bonjour " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nNous vous confirmons la réservation des formules suivantes : \n" . $reservations . "\nNous vous attendons le " . $formatter->format(new DateTime($datas['datePicker'])) . " à " . DateTime::createFromFormat('H:i:s', $datas['heure'])->format('H\hi') . " pour profiter de votre séjour.\n\nCordialement,\nL'équipe de chez Siny'art\n\nPour toute demande, contactez sinyart@gmail.com.",
                     $headers
                 );
             } else {
                 mail(
                     $datas['mail'],
                     mb_encode_mimeheader("Reservation(s) done", 'UTF-8', 'B', "\r\n"),
-                    "Hello " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nWe confirm you the reservation of the following formules : \n" . $reservations . "\nWe are waiting for you on " . $formatter->format(new DateTime($datas['datePicker'])) . " at " . DateTime::createFromFormat('H:i:s', $datas['heure'])->format('H:i A') . " to enjoy your stay.\n\nBest regards,\nSiny'art team",
+                    "Hello " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nWe confirm you the reservation of the following formules : \n" . $reservations . "\nWe are waiting for you on " . $formatter->format(new DateTime($datas['datePicker'])) . " at " . DateTime::createFromFormat('H:i:s', $datas['heure'])->format('H:i A') . " to enjoy your stay.\n\nBest regards,\nSiny'art team\n\nFor any request, contact sinyart@gmail.com.",
                     $headers
                 );
             }
@@ -121,14 +121,14 @@ class Manager
                 mail(
                     $user->getMail(),
                     mb_encode_mimeheader("Inscription réussie", 'UTF-8', 'B', "\r\n"),
-                    "Bonjour " . $user->getPrenom() . " " . $user->getNom() . ",\n\nNous vous confirmons votre inscription sur notre site.\nVous pouvez dès à présent réserver vos places pour l'exposition \"Tamara de Lempicka - Les années folles\".\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette inscription, veuillez nous contacter.",
+                    "Bonjour " . $user->getPrenom() . " " . $user->getNom() . ",\n\nNous vous confirmons votre inscription sur notre site.\nVous pouvez dès à présent réserver vos places pour l'exposition \"Tamara de Lempicka - Les années folles\".\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette inscription, veuillez nous contacter.\n\nPour toute demande, contactez sinyart@gmail.com.",
                     $headers
                 );
             } else {
                 mail(
                     $user->getMail(),
                     mb_encode_mimeheader("Registration done", 'UTF-8', 'B', "\r\n"),
-                    "Hello " . $user->getPrenom() . " " . $user->getNom() . ",\n\nWe confirm you your registration on our website.\nYou can now book your places for the exhibition \"Tamara de Lempicka - The Roaring Twenties\".\n\nBest regards,\nSiny'art team\n\nIf you are not the one who registered, please contact us.",
+                    "Hello " . $user->getPrenom() . " " . $user->getNom() . ",\n\nWe confirm you your registration on our website.\nYou can now book your places for the exhibition \"Tamara de Lempicka - The Roaring Twenties\".\n\nBest regards,\nSiny'art team\n\nIf you are not the one who registered, please contact us.\n\n For any request, contact sinyart@gmail.com.",
                     $headers
                 );
             }
@@ -183,14 +183,14 @@ class Manager
                 mail(
                     $mail,
                     mb_encode_mimeheader("Réinitialisation de votre mot de passe", 'UTF-8', 'B', "\r\n"),
-                    "Bonjour " . $usr['prenom'] . " " . $usr['nom'] . ",<br><br>Nous vous confirmons la demande de réinitialisation de votre mot de passe.<br>Vous pouvez le réinitialiser au lien ci dessous : <a href='" . $link . "'>" . $link . "</a><br><br>Cordialement,<br>L'équipe de chez Siny'art<br><br>Si vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.",
+                    "Bonjour " . $usr['prenom'] . " " . $usr['nom'] . ",<br><br>Nous vous confirmons la demande de réinitialisation de votre mot de passe.<br>Vous pouvez le réinitialiser au lien ci dessous : <a href='" . $link . "'>" . $link . "</a><br><br>Cordialement,<br>L'équipe de chez Siny'art<br><br>Si vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.\n\nPour toute demande, contactez sinyart@gmail.com.",
                     $headers
                 );
             } else {
                 mail(
                     $mail,
                     mb_encode_mimeheader("Reset your password", 'UTF-8', 'B', "\r\n"),
-                    "Hello " . $usr['prenom'] . " " . $usr['nom'] . ",<br><br>We confirm to you your password reset request.<br>You can reset it at the following link : <a href='" . $link . "'>" . $link . "</a><br><br>Best regards,<br>Siny'art team<br><br>If you are not the one who asked for this, please contact us.",
+                    "Hello " . $usr['prenom'] . " " . $usr['nom'] . ",<br><br>We confirm to you your password reset request.<br>You can reset it at the following link : <a href='" . $link . "'>" . $link . "</a><br><br>Best regards,<br>Siny'art team<br><br>If you are not the one who asked for this, please contact us.\n\nFor any request, contact sinyart@gmail.com.",
                     $headers
                 );
             }
@@ -238,14 +238,14 @@ class Manager
                 mail(
                     $reset['mail'],
                     mb_encode_mimeheader("Modification de votre mot de passe", 'UTF-8', 'B', "\r\n"),
-                    "Bonjour " . $reset['prenom'] . " " . $reset['nom'] . ",\n\nNous vous confirmons de la modification de votre mot de passe.\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.",
+                    "Bonjour " . $reset['prenom'] . " " . $reset['nom'] . ",\n\nNous vous confirmons de la modification de votre mot de passe.\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.\n\nPour toute demande, contactez sinyart@gmail.com.",
                     $headers
                 );
             } else {
                 mail(
                     $reset['mail'],
                     mb_encode_mimeheader("Modification of your password", 'UTF-8', 'B', "\r\n"),
-                    "Hello " . $reset['prenom'] . " " . $reset['nom'] . ",\n\nWe confirm you the modification of your password.\n\nBest regards,\nSiny'art team \n\nIf you are not the one who asked for this, please contact us.",
+                    "Hello " . $reset['prenom'] . " " . $reset['nom'] . ",\n\nWe confirm you the modification of your password.\n\nBest regards,\nSiny'art team \n\nIf you are not the one who asked for this, please contact us.\n\nFor any request, contact sinyart@gmail.com.",
                     $headers
                 );
             }
@@ -282,14 +282,14 @@ class Manager
                 mail(
                     $datas['mail'],
                     mb_encode_mimeheader("Modification de vos informations de compte", 'UTF-8', 'B', "\r\n"),
-                    "Bonjour " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nNous vous confirmons de la modification d'informations sur votre compte.\n\nCordialement,\nL'équipe de chez Siny'art",
+                    "Bonjour " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nNous vous confirmons de la modification d'informations sur votre compte.\n\nCordialement,\nL'équipe de chez Siny'art\n\nPour toute demande, contactez sinyart@gmail.com.",
                     $headers
                 );
             } else {
                 mail(
                     $datas['mail'],
                     mb_encode_mimeheader("Modification of your account informations", 'UTF-8', 'B', "\r\n"),
-                    "Hello " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nWe confirm you the modification of your account informations.\n\nBest regards,\nSiny'art team",
+                    "Hello " . $datas['prenom'] . " " . $datas['nom'] . ",\n\nWe confirm you the modification of your account informations.\n\nBest regards,\nSiny'art team\n\nFor any request, contact sinyart@gmail.com.",
                     $headers
                 );
             }
@@ -316,14 +316,14 @@ class Manager
             mail(
                 $mail,
                 mb_encode_mimeheader("Confirmation de la suppression de votre compte", 'UTF-8', 'B', "\r\n"),
-                "Bonjour,\n\nNous vous confirmons la suppression de votre compte.\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.",
+                "Bonjour,\n\nNous vous confirmons la suppression de votre compte.\n\nCordialement,\nL'équipe de chez Siny'art\n\nSi vous n'êtes pas à l'origine de cette demande, veuillez nous contacter.\n\nPour toute demande, contactez sinyart@gmail.com.",
                 $headers
             );
         } else {
             mail(
                 $mail,
                 mb_encode_mimeheader("Confirmation of the deletion of your account", 'UTF-8', 'B', "\r\n"),
-                "Hello,\n\nWe confirm you the deletion of your account.\n\nBest regards,\nSiny'art team\n\nIf you are not the one who asked for this, please contact us.",
+                "Hello,\n\nWe confirm you the deletion of your account.\n\nBest regards,\nSiny'art team\n\nIf you are not the one who asked for this, please contact us.\n\nFor any request, contact sinyart@gmail.com.",
                 $headers
             );
         }
