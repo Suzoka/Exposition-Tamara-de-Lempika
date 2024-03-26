e8665bfc4cd6132d08eb8ec3fd641f17aaffd575
Suzoka/Morgan <mor9an77@gmail.com>
2024-03-26 20:22:41 +0100

    :sparkles: navBar underligne

--- reservationWebsite/views/accueil.php
+++ reservationWebsite/views/accueil.php
@@ -1,25 +1,36 @@
 <head>
-    <meta name="description" content="Découvrez l'exposition Tamara de Lempicka - les années folles : une immersion dans les années folles à travers les tableaux de Tamara de Lempicka. Explorez les œuvres uniques de l'artiste dans cette exposition immersive unique jusqu'au 28 Avril 2024.">
+    <meta name="description"
+        content="Découvrez l'exposition Tamara de Lempicka - les années folles : une immersion dans les années folles à travers les tableaux de Tamara de Lempicka. Explorez les œuvres uniques de l'artiste dans cette exposition immersive unique jusqu'au 28 Avril 2024.">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles/style_h&f.css">
     <link rel="stylesheet" href="../styles/style_accueil.css">
-    <title><?php echo $_SESSION["lang"] == "fr" ? "Expo Tamara de Lempicka - Les années folles" : "Exhibition Tamara de Lempicka - The Roaring Twenties" ?></title>
+    <title>
+        <?php echo $_SESSION["lang"] == "fr" ? "Expo Tamara de Lempicka - Les années folles" : "Exhibition Tamara de Lempicka - The Roaring Twenties" ?>
+    </title>
     <link rel="icon" type="image/png" href="../img/favicon.png">
 </head>
 
 <body>
-    <a href="#content" class="skip-link"><?php echo $_SESSION["lang"] == "fr" ? "Aller au contenu" : "Go to content" ?></a>
+    <a href="#content" class="skip-link">
+        <?php echo $_SESSION["lang"] == "fr" ? "Aller au contenu" : "Go to content" ?>
+    </a>
     <?php include './views/components/header.php'; ?>
     <main id="content">
         <section class="banniere">
             <div class="text">
                 <h1><span id="tam">Tamara</span>
-                    <br>de Lempicka, <span id="era"><?php echo $_SESSION["lang"] == "fr" ? "les annees folles" : "the Roaring Twenties" ?></span>
+                    <br>de Lempicka, <span id="era">
+                        <?php echo $_SESSION["lang"] == "fr" ? "les annees folles" : "the Roaring Twenties" ?>
+                    </span>
                 </h1>
-                <h2><?php echo $_SESSION["lang"] == "fr" ? "28 mars - 28 avril 2024" : "March 28 - April 28, 2024" ?></h2>
-                <p><?php echo $_SESSION["lang"] == "fr" ? "Une exposition virtuelle dans les temps des années folles à travers les tableaux décalés et interrogateurs de Tamara." : "A virtual exhibition in the Roaring Twenties through Tamara's quirky and questioning paintings." ?></p>
+                <h2>
+                    <?php echo $_SESSION["lang"] == "fr" ? "28 mars - 28 avril 2024" : "March 28 - April 28, 2024" ?>
+                </h2>
+                <p>
+                    <?php echo $_SESSION["lang"] == "fr" ? "Une exposition virtuelle dans les temps des années folles à travers les tableaux décalés et interrogateurs de Tamara." : "A virtual exhibition in the Roaring Twenties through Tamara's quirky and questioning paintings." ?>
+                </p>
             </div>
             <div class="imgBanniere">
                 <img id="tamaraPaint" src="../img/TamaraPaint.png" alt="">
@@ -29,10 +40,11 @@
             </a>
         </section>
 
-        <section class="tamara">
+        <section class="tamara" data-scroll="Teaser">
             <h2>Tamara de Lempicka</h2>
             <div class="contentTamara">
-                <p class="t1"><?php echo $_SESSION["lang"] == "fr" ? "Tamara Rozalia Gurwick-Gorska, plus connue sous le nom de Tamara de Lempicka, a marqué l'histoire de l'art du XXe siècle par son talent artistique original et sa vie hors du commun. Née le 16 mai 1898 à Varsovie, elle est élevée dans un milieu aisé et cultivé, partageant son temps entre Saint-Pétersbourg et Lausanne. 
+                <p class="t1">
+                    <?php echo $_SESSION["lang"] == "fr" ? "Tamara Rozalia Gurwick-Gorska, plus connue sous le nom de Tamara de Lempicka, a marqué l'histoire de l'art du XXe siècle par son talent artistique original et sa vie hors du commun. Née le 16 mai 1898 à Varsovie, elle est élevée dans un milieu aisé et cultivé, partageant son temps entre Saint-Pétersbourg et Lausanne. 
                 <br>En 1914 elle entreprend une réelle carrière artistique en s'inscrivant à l'Académie des Beaux-Arts de Saint-Pétersbourg. En 1916 elle épouse l'avocat Tadeusz Lempicki et donne naissance à leur fille Marie-Chrisitine. En 1917 elle fuit la révolution d'octobre et se rend à Paris où elle est accueilli par ses cousins." : "Tamara Rozalia Gurwick-Gorska, better known as Tamara de Lempicka, left her mark on twentieth-century art history with her orignal artistic talent and her extraordinary life. 
                 Born in Warsaw on May 16, 1898, she grew up in an affluent and cultured environment, dividing her time between St Petersburg and Lausanne. 
                 <br>In 1914 she begin a real artistic career, by registering at the  Saint Petersburg Academy of Fine Arts. In 1916 she married the lawyer Tadeusz Lempicki and had her daughter Marie-Christine.
@@ -41,19 +53,21 @@
                 <img class="imgBig" src="../img/tamaraAccueil2.png" alt="">
                 <img class="imgSmall s1" src="../img/tamara.png" alt="">
                 <img class="imgSmall s2" src="../img/tamaraAccueil.png" alt="">
-                <p class="t2"><?php echo $_SESSION["lang"] == "fr" ? "Sous la tutelle de Maurice Denis et André Lhote, Tamara de Lempicka trouve un précieux soutien à l'Académie Ranson et de la Grande Chaumière. Fusionnant la Renaissance et le néo-cubisme, elle crée un style novateur, évoquant l'élégance des Années folles.
+                <p class="t2">
+                    <?php echo $_SESSION["lang"] == "fr" ? "Sous la tutelle de Maurice Denis et André Lhote, Tamara de Lempicka trouve un précieux soutien à l'Académie Ranson et de la Grande Chaumière. Fusionnant la Renaissance et le néo-cubisme, elle crée un style novateur, évoquant l'élégance des Années folles.
                 <br>Sa première exposition en 1923, \"Perspective (ou les deux amies)\", lance sa renommée et l'essor de l'Art Déco." : "Under the tutelage of Maurice Denis and André Lhote, Tamara de Lempicka found support at the Ranson Academy and the Grande Chaumière. Mixing the Renaissance and neo-Cubism, she created an innovative style, evoking the elegance of the Roaring Twenties.
                 <br>Her 1rst exhbition in 1923 \"Perspective (ou les deux amies)\", propelled her reputation and the rise of Art Deco." ?>
                 </p>
             </div>
         </section>
 
-        <section class="teaser">
+        <section class="teaser" data-scroll="Teaser">
             <h2>Teaser</h2>
             <div class="contentTeaser">
                 <div class="iframeResponsive">
                     <iframe src="https://www.youtube.com/embed/0AwAFKBY-Uo?si=6qMXA8hfmywyXxjk">
-                        <p>Ceci est un lien vers le teaser video de l'exposition que vous pouvez retrouver sur ce lien Youtube : <a href="https://youtu.be/0AwAFKBY-Uo?si=wH2p-N0rZnrZrEOA"></a></p>
+                        <p>Ceci est un lien vers le teaser video de l'exposition que vous pouvez retrouver sur ce lien
+                            Youtube : <a href="https://youtu.be/0AwAFKBY-Uo?si=wH2p-N0rZnrZrEOA"></a></p>
                     </iframe>
                 </div>
                 <div class="presentation">
@@ -67,7 +81,8 @@
                             <p>30min</p>
                         </div>
                     </div>
-                    <p id="textPresent"><?php echo $_SESSION["lang"] == "fr" ? "Plongez à la découverte des années folles à travers Tamara de Lempicka et ses tableaux ! Cette époque où l’art moderne et la représentation de la femme prennent vie. Les années folles symbole, d’une époque unique, euphorique, où le domaine artistique et culturelle connaissent leur apogée.
+                    <p id="textPresent">
+                        <?php echo $_SESSION["lang"] == "fr" ? "Plongez à la découverte des années folles à travers Tamara de Lempicka et ses tableaux ! Cette époque où l’art moderne et la représentation de la femme prennent vie. Les années folles symbole, d’une époque unique, euphorique, où le domaine artistique et culturelle connaissent leur apogée.
                         <br>Des œuvres qui illustrent l’émancipation de la femme et la beauté féminine. Tamara y peint
                         une image de la femme libre, autonome et épanoui dans sa vie et ses propres choix. Grâce à cette
                         exposition interactive unique, vous aurez la chance de vous inviter dans l’univers de Tamara et
@@ -76,14 +91,18 @@
                     </p>
                     <a id="reservation" href="./billetterie">
                         <img src="../img/icons/ticket.svg" alt="">
-                        <p><?php echo $_SESSION["lang"] == "fr" ? "Réserver" : "Book now" ?></p>
+                        <p>
+                            <?php echo $_SESSION["lang"] == "fr" ? "Réserver" : "Book now" ?>
+                        </p>
                     </a>
                 </div>
             </div>
         </section>
 
-        <section class="access" id="access">
-            <h2><?php echo $_SESSION["lang"] == "fr" ? "Accès" : "Access" ?></h2>
+        <section class="access" id="access" data-scroll="Access">
+            <h2>
+                <?php echo $_SESSION["lang"] == "fr" ? "Accès" : "Access" ?>
+            </h2>
             <div class="contentAccess">
                 <div class="plan">
                     <iframe
@@ -97,7 +116,9 @@
                     <div class="infoAccess">
                         <div class="h3">
                             <img src="../img/icons/transport.svg" alt="">
-                            <h3><?php echo $_SESSION["lang"] == "fr" ? "Venir en transport" : "By transport" ?></h3>
+                            <h3>
+                                <?php echo $_SESSION["lang"] == "fr" ? "Venir en transport" : "By transport" ?>
+                            </h3>
                         </div>
                         <ul>
                             <li>RER A - Noisy-Champs</li>
@@ -108,8 +129,10 @@
                     <div class="infoAccess">
                         <div class="h3">
                             <img src="../img/icons/voiture.svg" alt="">
-                            <h3><?php echo $_SESSION["lang"] == "fr" ? "Venir en voiture" : "By car" ?></h3>
-                        </div>    
+                            <h3>
+                                <?php echo $_SESSION["lang"] == "fr" ? "Venir en voiture" : "By car" ?>
+                            </h3>
+                        </div>
                         <ul>
                             <li>Via A4 </li>
                             <li>Bd du Ru du Nesles N370</li>
@@ -119,7 +142,9 @@
                     <div class="infoAccess">
                         <div class="h3">
                             <img src="../img/icons/velo.svg" alt="">
-                            <h3><?php echo $_SESSION["lang"] == "fr" ? "Venir en vélo" : "By bike" ?></h3>
+                            <h3>
+                                <?php echo $_SESSION["lang"] == "fr" ? "Venir en vélo" : "By bike" ?>
+                            </h3>
                         </div>
                         <ul>
                             <li>Via Av. de Gravelle</li>
@@ -130,7 +155,9 @@
                     <div class="infoAccess">
                         <div class="h3">
                             <img src="../img/icons/pied.svg" alt="">
-                            <h3><?php echo $_SESSION["lang"] == "fr" ? "Venir à pied" : "On foot" ?></h3>
+                            <h3>
+                                <?php echo $_SESSION["lang"] == "fr" ? "Venir à pied" : "On foot" ?>
+                            </h3>
                         </div>
                         <ul>
                             <li>Via D120</li>
@@ -143,6 +170,8 @@
         </section>
     </main>
     <?php include './views/components/footer.php'; ?>
+
+    <script src="../scripts/accueil.js" type="module"></script>
 </body>
 
 </html>
